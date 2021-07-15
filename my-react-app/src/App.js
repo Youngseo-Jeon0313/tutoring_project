05a2e2f21476a245f5482d8fa9b7c2a3546d94ae
전영서 <20wjsdudtj@gmail.com>
2021-07-16 01:11:50 +0900

    react_component

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,8 +1,32 @@
+import React, {Component} from 'react';
 import './App.css';
-import MainPageComponent from "./main"
 
-function App() {
-  return <MainPageComponent />;
+
+
+class TOC extends Component {
+  render() {
+    return (
+      <nav>
+        <ul>
+          <li><a href="1.html">질문 올리기</a></li>
+          <li><a href="1.html">그날그날 공부일기</a></li>
+          <li><a href="1.html">오늘의 글귀 적어보기</a></li>
+        </ul>
+      </nav>
+    );
+  }
 }
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App"> 
+    <TOC></TOC>
+      </div>
+    );
+  }
+}
+
+
 
 export default App;
