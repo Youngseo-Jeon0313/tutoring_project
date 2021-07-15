e2ef8ca1ad155c80828e7484c77665ef4ff8b954
전영서 <20wjsdudtj@gmail.com>
2021-07-15 23:51:32 +0900

    html_to_react

--- index/javabasis.js
+++
@@ -1,9 +0,0 @@
-//java 기본 배우기
-
-var name; //undefined (이 상태는 undefined라고 읽힌다.)
-name = "그랩" 
-var age= 27;
-var isFool = false; //isFool은 ..아마도 camel타입
-var nully = null; //null이라는 건 값이 없다!
-
-console.log(typeof Name)
