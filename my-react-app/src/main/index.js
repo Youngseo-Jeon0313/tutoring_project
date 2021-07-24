fd712f43c1bce4f664ade539fa3e39a7dfca3cbc
전영서 <20wjsdudtj@gmail.com>
2021-07-24 09:26:06 +0900

    upload_area

--- my-react-app/src/main/index.js
+++ my-react-app/src/main/index.js
@@ -1,12 +1,28 @@
 import {React} from 'react';
 import Container from '../dday';
-import { Input} from 'antd';
 import {DatePicker} from 'antd';
 import Container1 from '../dday9평';
+import {useParams } from "react-router-dom";
+import axios from "axios";
+import {useEffect, useState} from "react";
 
 
 function MainPage() {
+    const {id} = useParams();
+    const [content, setContent] = useState([]);
+    useEffect(function(){
+        axios.get(`http://localhost:8080/student/${id}`)
+        .then(function(result){
+            setContent(result.data.content);
+        })
+        .catch(function (error){
+            console.error(error);
+        });
+    })
 
+    if (content === null){
+        return <h1>정보가 없어유</h1>
+    }
 
     return (
 
@@ -28,13 +44,21 @@ function MainPage() {
     </main>
     <main>
         <span class ="본문">
+            
         <div class="제목">질문/피드백 공간
-            <Input placeholder="Basic usage" size ="large"/>
+        
+        <div>
+        <div id="image-box">
+        <img src={"/" + content.imageUrl} /></div>
+        <div id="question-box">
+        <div id="pageandnum">{content.pageandnum}</div>
+        <div id="createdAt">{content.createdAt}</div>
+        </div>
+        
+        </div>
         </div>
         </span>
     </main>
-
-
 </div>
 
     );
