8bcbca44aeae9b5755654075f2fb7e732df22573
전영서 <20wjsdudtj@gmail.com>
2021-07-31 20:41:36 +0900

    upload area

---
+++ my-react-app/src/config/constants.js
@@ -0,0 +1,2 @@
+//react program에서 다양한 변수들을 저장해놓고 쉽게 꺼내 쓰는 공간
+export const API_URL = 'http://localhost:8080';
--- my-react-app/src/student/index.js
+++ my-react-app/src/student/index.js
@@ -1,22 +1,74 @@
-import React from 'react';
+import {React, useState, } from 'react';
+import axios from "axios";
+import { API_URL } from "../config/constants";
+import './index.css';
 import {Form} from 'antd';
 
 
 function StudentPage() {
 
+
+      const [imageUrl, setImageUrl] = useState(null);
       const onSubmit = (values) => {
-        console.log(values);
-      }
+        axios.post(`${API_URL}/contents`,{
+          imageUrl: imageUrl,
+          description: values.description,
+          pageandnum: values.pageandnum,
+          date: values.date
+        })
+        .then((result)=> {
+          console.log(result);
+        });
+      };
+
+      const onChangeImage = (info) => {
+        if (info.file.status === "uploading") {
+          return;
+        }
+        if(info.file.status === "done") {
+          const response = info.file.response;
+          const imageUrl = response.imageUrl;
+          setImageUrl(imageUrl);
+        }
+      };
+
+     // --> 이 위에 있는 거는 일단 서버에서 가져온 거 쓰는 건데 일단은 보류 시키고 
+  
       return (
        <div>
          <body>
-         <Form name="사진 업로드" onFinish={onSubmit}>
-          <Form.Item name="upload">
+           <div> 
+             <Form name='질문 업로드' onFinish={onSubmit}>
+              <Form.Item 
+              name="upload"
+              label={<div className="upload-label">질문 사진</div>}
+              >
+                <Upload
+                  name="image"
+                  action={`${API_URL}/image`}
+                  listType="picture"
+                  showUploadList={false}
+                  onChange={onChangeImage}
+                  >
+                    {imageUrl ? (
+                      <img id="upload-img" src={`${API_URL}/${imageUrl}`} />
+                    ):(
+                      <div id="upload-img-placeholder">
+                        <img src="/images/"
+                      </div>
+                    
+                    
+                    )}
+
+                </Upload>
+              </Form.Item>
+             </Form>
+             
+          </div>
+           
             <div id="upload-img-placeholder">
               <span>하고 싶은 작업을 밑에서 선택하세요</span>
             </div>
-          </Form.Item>
-         </Form>
             <li><a href="./question">질문 올리기</a></li>
             <li><a href="/studydiary">그날그날 공부일기</a></li>
             <li><a href="/moto">오늘의 글귀 적어보기</a></li>
@@ -27,3 +79,9 @@ function StudentPage() {
   }
 
 export default StudentPage;
+
+
+
+
+
+
