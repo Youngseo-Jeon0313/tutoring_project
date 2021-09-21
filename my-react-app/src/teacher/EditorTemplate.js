6e96ec2c101326a9ed337cbcce39a25c920d72d7
Youngseo-Jeon0313 <20wjsdudtj@gmail.com>
2021-09-21 20:21:12 +0900

    controller_using_flexpercentage

--- my-react-app/src/teacher/EditorTemplate.js
+++ my-react-app/src/teacher/EditorTemplate.js
@@ -2,15 +2,57 @@ import {React, Component} from 'react'
 import './EditorTemplate.css';
 
 class EditorTemplate extends Component {
+    state = {
+        leftPercentage: 0.5
+    }
+
+    handleMouseMove = (e) => {
+        this.setState({
+            leftPercentage: e.clientX / window.innerWidth
+        })
+    }
+
+    handleMouseUp = (e) => {
+        document.body.removeEventListener('mousemove', this.handleMouseMove);
+        window.removeEventListener('mouseup', this.handleMouseUp);
+
+    }
+
+    handleSeparatorMouseDown = (e) => {
+        document.body.addEventListener('mousemove', this.handleMouseMove);
+        window.addEventListener('mouseup', this.handleMouseUp);
+    }
+
+
+
+
+
     render() {
         const { homework, board } =this.props;
+        const {leftPercentage} =this.state;
+        const {handleSeparatorMouseDown} =this;
+
+        const leftStyle = {
+            flex: leftPercentage
+        }
+
+        const rightStyle = {
+            flex: 1 - leftPercentage
+        }
+
+        const separatorStyle = {
+            left: `${leftPercentage * 100}%`
+        }
+
         return (
             <div className="template">
-            <div className='homework'>
+            <div className='homework' style={leftStyle}>
                 {homework}
             </div>
-            <div className="board">
+            <div className="board" style={rightStyle}>
                 {board}
+            </div>
+            <div className="separator" style={separatorStyle} onMouseDown={handleSeparatorMouseDown}>
             </div>
             </div>
         )
